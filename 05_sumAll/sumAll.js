b4a715c7fc395709e4bf4058c22fd25bcbd7e690
Donald Neuterman <dneuterman@gmail.com>
2022-01-04 00:37:17 -0500

    exercise 05 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+  if (typeof(firstNum) != 'number' || typeof(secondNum) != 'number' || firstNum < 0 || secondNum < 0) {
+    return 'ERROR';
+  };
+  if (firstNum > secondNum) {
+    [firstNum, secondNum] = [secondNum, firstNum];
+  };
+  
+  let sum = 0;
+  for (i = firstNum; i <= secondNum; i++) {
+    sum += i;
+  }
 
+  return sum;
 };
 
 // Do not edit below this line
