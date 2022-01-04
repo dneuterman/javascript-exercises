c41edd167be5b255785582e244846e4cb3dd8656
Donald Neuterman <dneuterman@gmail.com>
2022-01-03 23:39:31 -0500

    exercise 02 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+  if (num < 0) {
+    return 'ERROR';
+  }
+  let newStr = "";
+  while (num > 0) {
+    newStr += str;
+    num--;
+  };
+  return newStr;
 };
 
 // Do not edit below this line
