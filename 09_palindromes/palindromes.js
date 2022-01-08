6897b7d245f11eaf37a831833148880f91cd0610
Donald Neuterman <dneuterman@gmail.com>
2022-01-08 15:44:32 -0500

    09 palindromes complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const convertStringToLetters = (str) => {
+  const re = /[^a-zA-Z]+/;
+  let newStr = str.split(re).filter(Boolean);
+  return newStr.join('').toLowerCase();
+}
 
+const reverseString = (str) => {
+  return str.split('').reverse().join('');
+}
+
+const palindromes = function (str) {
+  return convertStringToLetters(str) === reverseString(convertStringToLetters(str));
 };
 
 // Do not edit below this line
