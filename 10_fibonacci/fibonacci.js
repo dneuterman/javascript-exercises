555e8513a04ef0106195df4caa9f587aaba93111
Donald Neuterman <dneuterman@gmail.com>
2022-01-08 16:08:46 -0500

    10 fibonacci complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(value) {
+  let position = parseInt(value);
+  if (position <= 0) {
+    return 'OOPS'
+  }
+  let prev = 0;
+  let sum = 0;
+  let current = 1;
+  for (i=1; i<position; i++) {
+    sum = current + prev;
+    prev = current;
+    current = sum;
+  };
+  return current;
 };
 
 // Do not edit below this line
