f958cfbbd4d0cd9a53901a868bbf4d93784bfcfe
Donald Neuterman <dneuterman@gmail.com>
2022-01-08 14:50:21 -0500

    08 calculator complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+	return args.reduce((a, b) => {
+    return a + b
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  return args.reduce((a, b) => {
+    return a * b;
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+	let answer = 1;
+  for (i = 0; i < power; i++) {
+    answer *= number;
+  };
+  return answer;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  }
+
+  let answer = 1;
+  for (i = 1; i <= num; i++) {
+    answer *= i;
+  };
+	return answer;
 };
 
 // Do not edit below this line
