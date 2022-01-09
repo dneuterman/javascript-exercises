a16fb230229bed1fa3745eed477036239a59b9d0
Donald Neuterman <dneuterman@gmail.com>
2022-01-09 00:46:53 -0500

    exercise 12 complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const checkForDeath = (person) => {
+    if (!person.hasOwnProperty('yearOfDeath')) {
+      const currentYear = new Date();
+      return currentYear.getFullYear();
+    } else {
+      return person.yearOfDeath;
+    }
+  }
+  const calculateAge = (person) => {
+    const death = checkForDeath(person)
+    return death - person.yearOfBirth;
+  }
 
+  let oldestAge = calculateAge(people[0]);
+  let oldestPerson = people[0];
+  for (i = 1; i < people.length; i++) {
+    const age = calculateAge(people[i]);
+    if (age > oldestAge) {
+      oldestAge = age;
+      oldestPerson = people[i];
+    };
+  };
+
+  console.log(oldestPerson);
+  return oldestPerson;
 };
 
 // Do not edit below this line
